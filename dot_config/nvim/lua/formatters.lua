c870b533914b68dea6bc8513471c06f365b7a0cd
Dmitriy Antonov <deantonov@avito.ru>
2023-04-17 10:26:05 +0300

    Update .config/nvim/lua/formatters.lua

--- dot_config/nvim/lua/formatters.lua
+++ dot_config/nvim/lua/formatters.lua
@@ -10,7 +10,6 @@ local sources = {
 	null_ls.builtins.formatting.black,
 	null_ls.builtins.formatting.gofmt,
 	null_ls.builtins.formatting.goimports,
-	null_ls.builtins.formatting.prettier,
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.formatting.jq,
 }
