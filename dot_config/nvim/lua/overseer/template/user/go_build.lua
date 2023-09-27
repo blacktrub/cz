c1739da73e5cda0f50a28549c73175f5f423eebf
blacktrub <lightlocdog@gmail.com>
2023-09-27 23:08:43 +0300

    Update .config/nvim/lua/overseer/template/user/go_build.lua

--- dot_config/nvim/lua/overseer/template/user/go_build.lua
+++ dot_config/nvim/lua/overseer/template/user/go_build.lua
@@ -4,7 +4,7 @@ return {
 		-- Full path to current file (see :help expand())
 		--vim.fn.getcwd()
 		return {
-			cmd = { "go build -o bin/ ./..." },
+			cmd = { "go", "build", "-o", "bin/", "./..." },
 			components = { { "on_output_quickfix", open = true }, "default" },
 		}
 	end,
