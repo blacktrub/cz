7450408a81a2af29ba1a550bb47edb89441a9499
Dmitriy Antonov <deantonov@avito.ru>
2023-10-08 21:10:15 +0300

    Update .config/nvim/lua/setups.lua

--- dot_config/nvim/lua/setups.lua
+++ dot_config/nvim/lua/setups.lua
@@ -154,10 +154,10 @@ cmp.setup({
 		end,
 	},
 	sources = {
-		{ name = "nvim_lsp" },
 		{ name = "luasnip" },
+		{ name = "nvim_lsp" },
 	},
-	confirmation = {
+	completion = {
 		completeopt = "menu,menuone,noinsert",
 	},
 })
