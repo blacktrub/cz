4ee69498db1093a3ee0420f6d2f7eaf78e15e942
Dmitriy Antonov <deantonov@avito.ru>
2024-06-03 22:04:38 +0300

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -76,6 +76,12 @@ return {
 	-- 		disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil", "minifiles" },
 	-- 	},
 	-- },
+	--
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1000, -- We'd like this plugin to load first out of the rest
+		config = true, -- This automatically runs `require("luarocks-nvim").setup()`
+	},
 	{
 		"nvim-neorg/neorg",
 		-- build = ":Neorg sync-parsers",
