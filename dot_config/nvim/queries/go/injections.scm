4193770202b31e57f242791186fb20b8add8fc65
Dmitriy Antonov <deantonov@avito.ru>
2024-12-24 13:20:54 +0300

    Update .config/nvim/queries/go/injections.scm

--- dot_config/nvim/queries/go/injections.scm
+++ dot_config/nvim/queries/go/injections.scm
@@ -22,13 +22,13 @@
 ;   (#set! injection.language "sql"))
 
 ; neovim nightly 0.10
-([
-  (interpreted_string_literal_content)
-  (raw_string_literal_content)
-  ] @injection.content
- (#match? @injection.content "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
- (#offset! @injection.content 0 1 0 -1)
-(#set! injection.language "sql"))
+; ([
+;   (interpreted_string_literal_content)
+;   (raw_string_literal_content)
+;   ] @injection.content
+;  (#match? @injection.content "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
+;  (#offset! @injection.content 0 1 0 -1)
+; (#set! injection.language "sql"))
 
 ; a general query injection
 ([
